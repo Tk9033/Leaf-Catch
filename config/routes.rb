6ac6fb71a6b2055ac57274c4e25bdca290fb2a95
Tk9033 <koday021369@gmail.com>
2025-09-16 07:48:57 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,7 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+
+   # トップページをゲーム開始画面にする
+   root "posts#index"
 end
